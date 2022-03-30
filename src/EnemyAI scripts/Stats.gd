f60eb29bb2a65959717feea3b124435f43d8fc61
James Helgren <joh23@txstate.edu>
2022-03-30 14:36:48 -0500

    added coins & started on player money system

--- src/EnemyAI scripts/Stats.gd
+++ src/EnemyAI scripts/Stats.gd
@@ -1,5 +1,8 @@
 extends Node
 
+export(int) var max_money = 99999 setget set_max_money
+var money = max_money setget set_money
+
 #max health is capable of being increased outside of this
 
 export(int) var max_health = 1 setget set_max_health
@@ -8,6 +11,8 @@ var health = max_health setget set_health
 signal no_health
 signal health_changed(value)
 signal max_health_changed(value)
+signal no_money
+signal money_changed(value)
 
 func set_max_health(value):
 	max_health = value
@@ -20,5 +25,16 @@ func set_health(value):
 	if health <= 0:
 		emit_signal("no_health")
 
+func set_max_money(value):
+	max_money = value
+	self.money = min(money, max_money)
+	
+func set_money(value):
+	money = value
+	emit_signal("money_changed", money)
+	if money <= 0:
+		emit_signal("no_money")
+
 func _ready():
 	self.health = max_health
+	self.money = 300
