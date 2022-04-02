4ad4ebe7b7283725576a862f4171e0dae2fce55f
Galo Conde <galo.conde@txstate.edu>
2022-04-01 22:03:34 -0500

    Created/Added Hit And Stay Buttons

---
+++ StayButton.gd
@@ -0,0 +1,31 @@
+extends TextureButton
+
+
+#get node
+export (NodePath) var button_path
+onready var button = get_node(button_path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#create connection
+	#button.connect("toggled", self, "on_toggled")
+	button.connect("pressed", self, "on_pressed")
+	
+	#set button name
+	button.set_text("Galo's Button")
+	
+	#set toggle move
+	#button.set_toggle_mode(true)
+	#pass # Replace with function body.
+
+func on_toggled(pressed):
+	if(pressed):
+		print("button is pressed")
+	else:
+		print("goodbye")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func on_pressed():
+	print("version on_pressed")
