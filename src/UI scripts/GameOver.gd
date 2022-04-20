055dd5f301e1202d60833ad8f84722d049128b26
Brandon Abundis <aba63@txstate.edu>
2022-04-20 16:09:11 -0500

    added more ui functionality

--- src/UI scripts/GameOver.gd
+++ src/UI scripts/GameOver.gd
@@ -7,12 +7,13 @@ var stats = PlayerStats
 
 onready var selectorOne = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
 onready var selectorTwo = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selectorThree = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
 
 func _ready():
 	set_current_selection(0)
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_down") and current_selection < 1:
+	if Input.is_action_just_pressed("ui_down") and current_selection < 2:
 		current_selection += 1
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
@@ -29,15 +30,23 @@ func handle_selection(_current_selection):
 		get_tree().change_scene("res://Main.tscn")
 		queue_free()
 	elif _current_selection == 1:
+		stats.health = 4 #makes health equal to 4 to fix bug
+		print(stats.health)
+		get_tree().change_scene("res://Screens/StartScreen.tscn")
+		queue_free()
+	elif _current_selection == 2:
 		get_tree().quit()
 		
 
 func set_current_selection(_current_selection):
 	selectorOne.text = ""
 	selectorTwo.text = ""
+	selectorThree.text = ""
 	if _current_selection == 0:
 		selectorOne.text = ">"
 	elif _current_selection == 1:
+		selectorThree.text = ">"
+	elif _current_selection == 2:
 		selectorTwo.text = ">"
 
 
--- src/UI scripts/LevelMenu.gd
+++ src/UI scripts/LevelMenu.gd
@@ -4,7 +4,12 @@ onready var selectorOne = $CenterContainer/VBoxContainer/CenterContainer2/VBoxCo
 onready var selectorTwo = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
 onready var selectorThree = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
 
+onready var level_box = $ColorRect/ColorRect2
+
 var current_selection = 0
+
+var l1_position = Vector2(105, 80)
+var l2_position = Vector2(105, 110)
 
 func _ready():
 	set_current_selection(0)
@@ -18,6 +23,9 @@ func _process(delta):
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_enter"):
 		handle_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_escape"):
+			get_tree().change_scene("res://Screens/StartScreen.tscn")
+			queue_free()
 
 func handle_selection(_current_selection):
 	if _current_selection == 0:
@@ -36,7 +44,22 @@ func set_current_selection(_current_selection):
 	selectorThree.text = ""
 	if _current_selection == 0:
 		selectorOne.text = ">"
+		level_box.rect_position = Vector2(105, 80)
+		level_box.rect_size = Vector2(104, 17)
+		$Casino.visible = true
+		$Mythic.visible = false
+		$Dungeon.visible = false
 	elif _current_selection == 1:
 		selectorTwo.text = ">"
+		level_box.rect_position = Vector2(105, 99)
+		level_box.rect_size = Vector2(98, 17)
+		$Casino.visible = false
+		$Mythic.visible = true
+		$Dungeon.visible = false
 	elif _current_selection == 2:
 		selectorThree.text = ">"
+		level_box.rect_position = Vector2(105, 118)
+		level_box.rect_size = Vector2(109, 17)
+		$Casino.visible = false
+		$Mythic.visible = false
+		$Dungeon.visible = true
